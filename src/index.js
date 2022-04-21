a69351e43059c9d2e7a122b3cff6165ca91b19f4
kyunosaya <saya0203@naver.com>
2022-04-21 19:49:19 +0900

    start

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+//component
+
+//source
+import './index.css'
+
+
+function App(){
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+const root = document.getElementById('root')
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+
+)
